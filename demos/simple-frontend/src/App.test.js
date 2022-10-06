cf9c45922171291d9f1c4e0db143f22d8eeb72e7
psilore <psiloc@gmail.com>
2022-10-06 10:24:57 +0200

    updated workflow

--- demos/simple-frontend/src/App.test.js
+++ demos/simple-frontend/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { create } from "react-test-renderer";
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("snapshot test", () => {
+  test("testing app button", () => {
+    let tree = create(<App />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 });
