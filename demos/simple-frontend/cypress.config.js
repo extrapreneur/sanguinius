8e59de92fae5fd92f6200cf48d9d90470a2c9a13
Psilore <psilore@users.noreply.github.com>
2022-10-07 18:05:02 +0200

    Update cypress.config.js

--- demos/simple-frontend/cypress.config.js
+++ demos/simple-frontend/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "naouu8",
   videos: true,
   reporter: "cypress-multi-reporters",
   reporterOptions: {
